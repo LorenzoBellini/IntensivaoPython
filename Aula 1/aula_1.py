f63d624b8010d9d1a5ec677139f767e5722f0190
Lorenzo Bellini <lbellini226@gmail.com>
2026-03-03 16:45:13 -0300

    Inicializando o repositório no meu PC e subindo alguns arquivos

---
+++ Aula 1/aula_1.py
@@ -0,0 +1,9 @@
+"""
+Passo a passo:
+    Entrar no site;
+    Fazer login (email e senha);
+    Cadastrar produto:
+        Abrir planilha para consulta;
+        Preencher os campos no site;
+        Repetir o processo para cada produto.
+"""
